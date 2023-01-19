dd90a41896528946a82993e39d7849acebe19dfa
Vaticinator <barnaba@cat-astrophe-games.com>
2023-01-19 13:23:32 +0100

    Add option "Revert to default" for online json editor

--- Backend/index.php
+++ Backend/index.php
@@ -6,28 +6,32 @@ $error = '';
 
 /* save game json */
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $json_str = preg_replace('/[^A-Za-z0-9\-\_\{\}\[\]\,\"\:\.\ \t\r\n]/', '', $_POST["jconf"]);
-	//echo $json_str;
-	
+    
+    if($_POST["submit"] == "Revert to default"){
+        $json_str = file_get_contents('settings_default.json');
+    }
+    else{
+        $json_str = preg_replace('/[^A-Za-z0-9\-\_\{\}\[\]\,\"\:\.\ \t\r\n]/', '', $_POST["jconf"]);
+    }
 
     $json = json_decode($json_str);
     if(json_last_error() === JSON_ERROR_NONE){
 
-	
-		$fp = fopen($file, 'w');  
-		if(!$fp) {
-			throw new Exception('File open failed.');
-		}
-			
-		if(flock($fp, LOCK_EX)){
-			fwrite($fp, $json_str);
-			flock($fp, LOCK_UN);
-		}
-		fclose($fp);
-	}
-	else{
-		$error = '&nbsp; UNSAVED. JSON parsing error';
-	}
+    
+        $fp = fopen($file, 'w');  
+        if(!$fp) {
+            throw new Exception('File open failed.');
+        }
+            
+        if(flock($fp, LOCK_EX)){
+            fwrite($fp, $json_str);
+            flock($fp, LOCK_UN);
+        }
+        fclose($fp);
+    }
+    else{
+        $error = '&nbsp; UNSAVED. JSON parsing error';
+    }
 }
     
 ?>
@@ -46,7 +50,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   <p><label for="jconf">JSON:</label></p>
   <textarea id="jconf" name="jconf" rows="20" cols="80"><?php echo $json_str; ?></textarea>
   <br>
-  <input type="submit" value="Save"><?php echo $error; ?>
+  <input type="submit" name="submit" value="Save"> <input type="submit" name="submit" value="Revert to default"> <?php echo $error; ?><?php echo $error; ?>
 </form>
 </body>
 </html>
