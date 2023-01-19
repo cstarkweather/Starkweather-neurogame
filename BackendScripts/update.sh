ba53b665ce7eb82598da0e757299a48ac5fdb9e5
Vaticinator <barnaba@cat-astrophe-games.com>
2023-01-19 13:33:37 +0100

    settings.json to gitignore

--- BackendScripts/update.sh
+++ BackendScripts/update.sh
@@ -3,7 +3,12 @@ set -eu
 
 cd neuropace-behavioral-fps
 git pull --rebase
+
+if [ '!' -f ~/public_html/settings.json ]; then
+  cp ~/public_html/settings_default.json ~/public_html/settings.json
+  chmod a+rw- ~/public_html/settings.json
+fi
+
 chmod -R a+rx ~/public_html/
-chmod a+rw- ~/public_html/settings.json
 chmod a+rwx ~/public_html/logs/
 
